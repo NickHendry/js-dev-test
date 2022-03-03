faaf9d58cf4f05adda593f950e03187984ae76bb
Nicholas Hendry <nicholaskhendry@gmail.com>
2022-03-04 07:43:18 +1100

    Populated processOrder method so it validates the name+email is provided and the phone number is an integer. Added dependency to uuid-int npm package. Returns either errors or the new order ID from processOrder.

--- backend/modules/checkout/checkout.controller.js
+++ backend/modules/checkout/checkout.controller.js
@@ -1,5 +1,29 @@
+const UUID = require('uuid-int');
+
+const generator = UUID(0)
+
 function processOrder(req, res, next) {
+    const data = req.body;
+
+    let errors = [];
+
+    if (typeof data.name === 'undefined' || !data.name) {
+        errors.push({field: 'name', message: 'Name is required'});
+    }
+    if (typeof data.phone !== 'undefined' && !Number.isInteger(parseInt(data.phone))) {
+        errors.push({field: 'phone', message: 'Phone number must be an integer'});
+    }
+    if (typeof data.email === 'undefined' || !data.email) {
+        errors.push({field: 'email', message: 'Email is required'});
+    }
+
+    if (errors.length > 0) {
+        res.status(422).send(errors);
+    } else {
+        let newOrderId = generator.uuid();
     
+        res.send({newOrderId: `${newOrderId}`});
+    }
 }
   
 module.exports = {
