3a4e599aadc3953b2fa22cd3ae748239d62d5821
Nicholas Hendry <nicholaskhendry@gmail.com>
2022-03-04 07:04:47 +1100

    Populated the About page.

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,6 +1,19 @@
 <template>
-  <div id="about-view" class="p-4">
-    <h1>This is an about page!</h1>
+  <div id="about-view" class="p-4 flex flex-col items-center">
+    <h1 class="text-2xl font-bold">Simple Ecommerce Website</h1>
+    <div class="rounded-md w-1/2 shadow-md mt-5">
+      <h3 class="font-bold text-md">About this Site</h3>
+      <p>This site provides a convenient way to shop for your favourite shapes!</p>
+    </div>
+    <div class="rounded-md w-1/2 shadow-md mt-10">
+      <h3 class="font-bold text-md">Contact Us</h3>
+      <p>4 Banksia Street</p>
+      <p>Emu Plains</p>
+      <p>Penrith, Sydney</p>
+      <p>NSW 2750</p>
+      <p>Ph: 0437 541 323</p>
+      <p>Email: villageco@gmail.com</p>
+    </div>
     <router-link tag="button" :to="{ name: 'Home'}" class="bg-blue-400 w-24 h-10 text-white font-semibold rounded mt-4">Go home</router-link>
   </div>
 </template>
